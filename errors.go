370046760fd0b494659d0d11652b080dc4c71c41
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2021-01-18 20:52:02 +0100

    Update errors.go

--- errors.go
+++ errors.go
@@ -3,7 +3,7 @@ package rest
 import "errors"
 
 var (
-	ErrUnmarshal            = errors.New("UNMARSHAL_ERROR")
+	ErrUnmarshal    = errors.New("UNMARSHAL_ERROR")
 	ErrMissingField = errors.New("MISSING_FIELD")
-	ErrNotFound             = errors.New("NOT_FOUND")
+	ErrNotFound     = errors.New("NOT_FOUND")
 )
