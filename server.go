7c3cd65638a08d37a94efa2abd5ceebdf5d1c1e6
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2021-10-16 17:26:52 +0200

    feat: added option to run rest server in HTTPS mode

--- server.go
+++ server.go
@@ -2,43 +2,53 @@ package rest
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/pkg/errors"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
-// Server - http server struct
+type SSLConfig struct {
+	Port     int    // https server port
+	Redirect bool   // defines if http requests will be redirected to the https
+	URL      string // url where http requests will be redirected
+
+	CertPath string // path to the ssl certificate
+	KeyPath  string // path to the ssl key
+}
+
+// Server - rest server struct
 type Server struct {
+	Address string
 	Port    int
 	IsReady *atomic.Value
+	SSL     *SSLConfig
 
 	ReadHeaderTimeout time.Duration
 	WriteTimeout      time.Duration
 	IdleTimeout       time.Duration
 
-	httpServer *http.Server
-}
+	httpServer  *http.Server
+	httpsServer *http.Server
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte("."))
-}
-
-func ready(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte("."))
+	mu sync.Mutex
 }
 
 // Run - will initialize server and run it on provided port
 func (s *Server) Run(router http.Handler) error {
+	if s.Address == "*" {
+		s.Address = ""
+	}
 	if s.Port == 0 {
 		s.Port = 8080
 	}
+
 	if s.IsReady == nil {
 		s.IsReady = &atomic.Value{}
 		s.IsReady.Store(true)
@@ -57,40 +67,124 @@ func (s *Server) Run(router http.Handler) error {
 	if router == nil {
 		mux := chi.NewRouter()
 		mux.Use(Readiness("/readiness", s.IsReady))
-		mux.HandleFunc("/ping", handler)
-		mux.HandleFunc("/liveness", handler)
+		mux.HandleFunc("/ping", okHandler)
+		mux.HandleFunc("/liveness", okHandler)
 		router = mux
 	}
 
-	s.httpServer = &http.Server{
-		Addr:              fmt.Sprintf(":%d", s.Port),
-		Handler:           router,
-		ReadHeaderTimeout: s.ReadHeaderTimeout,
-		WriteTimeout:      s.WriteTimeout,
-		IdleTimeout:       s.IdleTimeout,
+	log.Printf("[INFO] http rest server on %s:%d", s.Address, s.Port)
+
+	httpRouter := router
+
+	if s.SSL != nil {
+		if s.SSL.Port == 0 {
+			s.SSL.Port = s.Port + 1
+		}
+
+		if s.SSL.Redirect {
+			mux := chi.NewRouter()
+			mux.Handle("/*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				newURL := s.SSL.URL + r.URL.Path
+				if r.URL.RawQuery != "" {
+					newURL += "?" + r.URL.RawQuery
+				}
+				http.Redirect(w, r, newURL, http.StatusTemporaryRedirect)
+			}))
+			httpRouter = mux
+
+			log.Printf("[INFO] http redirect server on %s:%d", s.Address, s.Port)
+		}
+
+		if _, err := os.Stat(s.SSL.CertPath); os.IsNotExist(err) {
+			return errors.Wrap(err, "ssl certificate file not found")
+		}
+		if _, err := os.Stat(s.SSL.KeyPath); os.IsNotExist(err) {
+			return errors.Wrap(err, "ssl key file not found")
+		}
+
+		log.Printf("[INFO] https rest server on %s:%d", s.Address, s.SSL.Port)
+
+		s.mu.Lock()
+		s.httpsServer = s.https(s.Address, s.SSL.Port, router)
+		s.mu.Unlock()
+
+		go func() {
+			log.Printf("[WARN] https server terminated, %s", s.httpsServer.ListenAndServeTLS(s.SSL.CertPath, s.SSL.KeyPath))
+		}()
 	}
 
-	log.Printf("[INFO] rest server started on %s", s.httpServer.Addr)
+	s.mu.Lock()
+	s.httpServer = s.http(s.Address, s.Port, httpRouter)
+	s.mu.Unlock()
 
 	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		return err
+		return errors.Wrap(err, "start http server")
 	}
 
 	return nil
 }
 
-// Shutdown - shutdown http server
+// Shutdown - shutdown rest server
 func (s *Server) Shutdown() error {
 	log.Print("[INFO] shutdown rest server")
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	if s.httpServer != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
 		if err := s.httpServer.Shutdown(ctx); err != nil {
+			return err
+		}
+	}
+	if s.httpsServer != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		if err := s.httpsServer.Shutdown(ctx); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (s *Server) http(address string, port int, router http.Handler) *http.Server {
+	return &http.Server{
+		Addr:              fmt.Sprintf("%s:%d", address, port),
+		Handler:           router,
+		ReadHeaderTimeout: s.ReadHeaderTimeout,
+		WriteTimeout:      s.WriteTimeout,
+		IdleTimeout:       s.IdleTimeout,
+	}
+}
+func (s *Server) https(address string, port int, router http.Handler) *http.Server {
+	server := s.http(address, port, router)
+	server.TLSConfig = &tls.Config{
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		},
+		MinVersion: tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+			tls.CurveP384,
+		},
+	}
+	return server
+}
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("."))
 }
--- server_test.go
+++ server_test.go
@@ -1,8 +1,8 @@
 package rest
 
 import (
+	"crypto/tls"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
@@ -18,66 +18,151 @@ func (h *handlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestServer_Run(t *testing.T) {
-	srv := &Server{
-		Port: 8091,
-	}
+	t.Run("default value", func(t *testing.T) {
+		t.Run("address *", func(t *testing.T) {
+			srv := &Server{
+				Address: "*",
+				Port:    8123,
+			}
 
-	handler := new(handlerFunc)
-	go func() {
-		require.NoError(t, srv.Run(handler))
-	}()
+			handler := new(handlerFunc)
+			go func() {
+				require.NoError(t, srv.Run(handler))
+			}()
 
-	req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%d", srv.Port), nil)
-	require.Nil(t, err)
+			req, err := http.Get(fmt.Sprintf("http://localhost:%d", srv.Port))
+			require.NoError(t, err)
+			require.NotNil(t, req)
 
-	client := &http.Client{Timeout: 5 * time.Second}
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "must be 200")
+			require.Equal(t, http.StatusOK, req.StatusCode)
+		})
+		t.Run("empty port", func(t *testing.T) {
+			srv := &Server{}
 
-	go func() {
-		time.Sleep(100 * time.Millisecond)
-		require.NoError(t, srv.Shutdown())
-	}()
+			handler := new(handlerFunc)
+			go func() {
+				require.NoError(t, srv.Run(handler))
+			}()
 
-	srv2 := &Server{
-		Port: 8091,
-	}
-	err = srv2.Run(nil)
-	require.Error(t, err)
-}
+			req, err := http.Get(fmt.Sprintf("http://localhost:8080"))
+			require.NoError(t, err)
+			require.NotNil(t, req)
 
-func TestServer_Run_EmptyRouter(t *testing.T) {
-	srv := &Server{
-		Port: 1234,
-	}
+			require.Equal(t, http.StatusOK, req.StatusCode)
+		})
+		t.Run("empty router", func(t *testing.T) {
+			srv := &Server{
+				Port: 1234,
+			}
+			defer func() {
+				require.NoError(t, srv.Shutdown())
+			}()
 
-	defer func() {
-		require.NoError(t, srv.Shutdown())
-	}()
+			go func() {
+				require.NoError(t, srv.Run(nil))
+			}()
 
-	go func() {
-		require.NoError(t, srv.Run(nil))
-	}()
+			host := fmt.Sprintf("http://localhost:%d", srv.Port)
+			resp, err := http.Get(host + "/ping")
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode)
 
-	host := fmt.Sprintf("http://localhost:%d", srv.Port)
-	resp, err := http.Get(host + "/ping")
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+			resp, err = http.Get(host + "/liveness")
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode)
 
-	resp, err = http.Get(host + "/liveness")
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+			resp, err = http.Get(host + "/readiness")
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode)
 
-	resp, err = http.Get(host + "/readiness")
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+			srv.IsReady.Store(false)
 
-	srv.IsReady.Store(false)
+			resp, err = http.Get(host + "/readiness")
+			require.NoError(t, err)
+			require.Equal(t, http.StatusServiceUnavailable, resp.StatusCode)
+		})
+	})
 
-	resp, err = http.Get(host + "/readiness")
-	require.NoError(t, err)
-	require.Equal(t, http.StatusServiceUnavailable, resp.StatusCode)
+	// go run $GOROOT/src/crypto/tls/generate_cert.go --rsa-bits 1024 --host 127.0.0.1,::1,localhost --ca --start-date "Jan 1 00:00:00 1970" --duration=1000000h
+	t.Run("SSL", func(t *testing.T) {
+		t.Run("no cert file *", func(t *testing.T) {
+			srv := &Server{
+				SSL: &SSLConfig{},
+			}
+			require.Error(t, srv.Run(nil))
+		})
+		t.Run("no key file *", func(t *testing.T) {
+			srv := &Server{
+				SSL: &SSLConfig{
+					CertPath: "./cert.pem",
+				},
+			}
+			require.Error(t, srv.Run(nil))
+		})
+		t.Run("run on default port", func(t *testing.T) {
+			srv := &Server{
+				Port: 5489,
+				SSL: &SSLConfig{
+					CertPath: "./cert.pem",
+					KeyPath:  "./key.pem",
+				},
+			}
+			defer func() {
+				require.NoError(t, srv.Shutdown())
+			}()
+			go func() {
+				require.NoError(t, srv.Run(nil))
+			}()
+
+			req, err := http.NewRequest("GET", "https://localhost:5490/ping", nil)
+			require.NoError(t, err)
+			require.NotNil(t, req)
+
+			client := &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+				},
+			}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			require.NotNil(t, req)
+
+			require.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+		t.Run("redirect http to https", func(t *testing.T) {
+			srv := &Server{
+				Port: 4816,
+				SSL: &SSLConfig{
+					Redirect: true,
+					URL:      "https://localhost:4817",
+					CertPath: "./cert.pem",
+					KeyPath:  "./key.pem",
+				},
+			}
+			defer func() {
+				require.NoError(t, srv.Shutdown())
+			}()
+			go func() {
+				require.NoError(t, srv.Run(nil))
+			}()
+
+			req, err := http.NewRequest("GET", "http://localhost:4816/ping?param=1", nil)
+			require.NoError(t, err)
+			require.NotNil(t, req)
+			client := &http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+				},
+			}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			require.NotNil(t, req)
+			require.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+		})
+	})
 }
 
 func TestServer_Shutdown(t *testing.T) {
@@ -87,7 +172,7 @@ func TestServer_Shutdown(t *testing.T) {
 	handler := new(handlerFunc)
 
 	go func() {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 		require.NoError(t, srv.Shutdown())
 	}()
 
